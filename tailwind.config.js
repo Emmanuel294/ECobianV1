5d20d9ade8274df8e4512c3d2fba8b9415ef4640
Emmanuel294 <emmanuelcobianz294@gmail.com>
2023-04-04 11:03:45 -0600

    Changed bg color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     colors: {
       'light-system-purple-1': '#5994FF',
       'light-system-purple-2': '#70E0FF',
-      'light-system-purple-3': '#3D4490',
+      'light-system-purple-3': '#22269E',
       'light-system-blue-1': '#83C3FF',
     },
     extend: {},
